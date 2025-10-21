afa76540cfe25008be5d19db323a3bec945848ef
Adrián Vilariño Vázquez <adrian.vilarinovazqu@WIRTZ.LOCAL>
2025-10-21 19:40:46 +0200

    Usuario 1 - Commit con el merge de feature suma ya realizado, suma.py y calculadora.py ya hechas

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,19 @@
+from suma import sumar
 def calculadora():
-    print("Calculadora")
+    print(""" 
+          Opciones:
+          0 - Salir
+          1 - Suma
+""")
+    while True:
+        opcion = int(input("Introduzca la opcion que quiera: "))
+        if opcion == 0:
+            print("Saliendo de calculadora...")
+            break
+        if opcion == 1:
+            a = int(input("Introduzca el primer numero: "))
+            b = int(input("Introduzca el segundo número: "))
+            print(f"El resultado de la suma de {a} + {b} es: {sumar(a, b)}")
 
 if __name__ == "__main__":
     calculadora()
