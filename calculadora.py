bdca6955750b238f6eda643ff4d0b9d28c5e73b7
Adrián Vilariño Vázquez <adrian.vilarinovazqu@WIRTZ.LOCAL>
2025-10-23 19:43:16 +0200

    Usuario 2 - Merge de feature_resta en develop para provocar el error

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,21 @@
+from resta import restar
+
 def calculadora():
-    print("Calculadora")
+    print("""
+    Opciones:
+    0 - Salir
+    2 - Resta
+    """)
+
+    while True:
+        opcion = int(input("Introduzca la opción: "))
+        if opcion == 0:
+            print("Saliendo...")
+            break
+        elif opcion == 2:
+            a = int(input("Primer número: "))
+            b = int(input("Segundo número: "))
+            print(f"Resultado de la resta: {restar(a, b)}")
 
 if __name__ == "__main__":
     calculadora()
